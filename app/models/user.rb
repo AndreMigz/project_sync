71b1215e193fc5486ca44b648082d88900edae17
AndreMigz <migzarre@gmail.com>
2024-11-02 22:57:15 +0800

    Add profile image and name on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_image
+
+  def fullname
+    if firstname.present? || lastname.present?
+      "#{firstname} #{lastname}".strip
+    else
+      email.split("@").first
+    end
+  end
 end
